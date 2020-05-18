5fb8277e733306128612677af0914658c09c9aa4
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-19 03:40:40 +0530

    refactor:card formatting and bug fixes.

--- app/src/main/java/com/learning/cardvalidation/MainActivity.kt
+++ app/src/main/java/com/learning/cardvalidation/MainActivity.kt
@@ -7,15 +7,20 @@ import android.text.InputFilter
 import android.text.InputType
 import android.text.TextWatcher
 import android.util.Log
+import android.view.Gravity
 import android.view.KeyEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import coil.api.load
 import com.learning.cardvalidation.Utils.CardFormatter
 import com.learning.cardvalidation.Utils.validateCardNumber
+import com.learning.cardvalidation.model.CardType
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.StringBuilder
 
@@ -28,12 +33,65 @@ class MainActivity : AppCompatActivity() {
         llRoot.focusedChild as EditText
     }
 
+    val _cardTypeLiveData = MutableLiveData<CardType>()
+
+    val cardTypeLiveData:LiveData<CardType> = _cardTypeLiveData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        createEditTexts(editTextCreationCount)
+        createEditTexts(editTextCreationCount, arrayOf(3,7,11))
+        cardTypeLiveData.observe(this, Observer {
+            onCardTypeChange(it)
+        })
     }
 
+    fun onCardTypeChange(cardType: CardType?){
+        when(cardType?.typeDrawable){
+            R.drawable.amex -> {
+                cardImage.load(getDrawable(cardType?.typeDrawable )) {}
+                formatEditText(cardType.spacingIndex, cardType.length)
+            }
+            R.drawable.mastercard -> {
+                cardImage.load(getDrawable(cardType?.typeDrawable )) {}
+                formatEditText(cardType.spacingIndex, cardType.length)
+            }
+            R.drawable.diners -> {
+                cardImage.load(getDrawable(cardType?.typeDrawable )) {}
+                formatEditText(cardType.spacingIndex, cardType.length)}
+            R.drawable.creditcard ->{
+                cardImage.load(getDrawable(cardType?.typeDrawable )) {}
+                formatEditText(cardType.spacingIndex, cardType.length)}
+
+            else -> {}
+        }
+    }
+
+    fun formatEditText(spacingIndices: Array<Int>, cardLength: Int){
+        for( index in 0..cardLength-1) {
+            Log.d("INDEX","Index is $index")
+            val editText = llRoot.getChildAt(index) as EditText
+            if(index in spacingIndices){
+                editText.setPadding(0,0,8,0)
+            }else{
+                editText.setPadding(0,0,0,0)
+            }
+
+            var difference =editTextCreationCount - cardLength
+            if(cardLength < editTextCreationCount){
+
+                editTextCreationCount = cardLength
+            }
+            Log.d("DELETING", "Diff $difference")
+
+            /*for( diffIndex in 1..difference){
+                Log.d("DELETING", "Delete diff index $diffIndex")
+                val editText = llRoot.getChildAt(editTextCreationCount-diffIndex) as EditText
+                editText.visibility = View.GONE
+            }*/
+
+        }
+    }
 
     fun createCustomEditText(): EditText {
         return EditText(this).apply {
@@ -67,11 +125,7 @@ class MainActivity : AppCompatActivity() {
             Log.d("String", "The character is: ${editable.toString()}")
             val currentEditText = llRoot.focusedChild as EditText
             val id = currentEditText.id
-           if(id == 4){
-               val params = currentEditText.layoutParams as ViewGroup.MarginLayoutParams
-               params.setMargins(2,2,4,2)
-               currentEditText.layoutParams = params
-           }
+
             Log.d("ID","Et id: $id and focused child ${llRoot.focusedChild.id}")
            if(id < editTextCreationCount) {
                if (editable?.length == 1) {
@@ -88,34 +142,36 @@ class MainActivity : AppCompatActivity() {
                getNextField(id, false)
                cardInput.deleteCharAt((cardInput.length) - 1)
            }
-           
+            _cardTypeLiveData.postValue(CardFormatter.matchPatternWithCardType(cardInput))
+
            if(id==editTextCreationCount-1 && editable?.length==1){
                checkForValidCard(cardInput.toString())
+           }else{
+               tvError.visibility = View.GONE
            }
         }
 
         override fun beforeTextChanged(char: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
-        override fun onTextChanged(char: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            val cardType = CardFormatter.matchPatternWithCardType(cardInput)
-            Log.d("image","CardType: ${cardType} charsequence ${char}")
-            cardImage.load(getDrawable(cardType?.typeDrawable ?: R.drawable.creditcard)) {}
-
-            }
-
+        override fun onTextChanged(char: CharSequence?, p1: Int, p2: Int, p3: Int) {}
         }
 
-    private fun createEditTexts(count: Int){
+    private fun createEditTexts(count: Int, spaceIndexes: Array<Int>){
         for(i in 0 until count){
             val layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
 
             val numberEditText = createCustomEditText()
             numberEditText.id = i
+            if(i in spaceIndexes){
+                numberEditText.setPadding(0,0,8,0)
+            } else {
+                numberEditText.setPadding(0,0,0,0)
+            }
+
             numberEditText.layoutParams = layoutParams
             numberEditText.addTextChangedListener(textWatcher)
             //numberEditText.setOnKeyListener(backSpaceKeyListener)
             llRoot.addView(numberEditText)
-
         }
 
     }
@@ -131,7 +187,6 @@ class MainActivity : AppCompatActivity() {
         }
         val editText = llRoot.getChildAt(position)?.let { it as EditText }
         editText?.requestFocus()
-        //TODO : Remove this
         editText?.isCursorVisible = true
     }
     
