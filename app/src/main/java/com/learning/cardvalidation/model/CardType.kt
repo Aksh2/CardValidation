9687d1d77750081189d875809ef6a73a5eb9da7a
Akshay Kumar H <akshay.h@pharmeasy.in>
2020-05-19 03:38:29 +0530

    refactor: Added additional parameters.

--- app/src/main/java/com/learning/cardvalidation/model/CardType.kt
+++ app/src/main/java/com/learning/cardvalidation/model/CardType.kt
@@ -1,5 +1,4 @@
 package com.learning.cardvalidation.model
 
-import android.graphics.drawable.Drawable
 
-data class CardType(val typeDrawable : Int, val spacingIndex: Int)
+data class CardType(val typeDrawable : Int, val spacingIndex: Array<Int>, val length: Int)
